cc3114b6dc0485e1fad79756826eb3357873dd1d
lucKulke <lucaskulke98@gmail.com>
2021-06-09 12:21:38 +0200

    completed tictactoe game and create rubocopconfig file

--- lesson_6/game_TicTacToe.rb
+++ lesson_6/game_TicTacToe.rb
@@ -1,23 +1,30 @@
+PLAYER_1_MARKER = "X"
+PLAYER_2_MARKER, COMPUTER_MARKER = "O","O"
+EMPTY_SQUARE = " "
+
+
 
 # METHODS
 
-def display_board(brd)
-	
-	puts "      BOARD"
-	puts "      -----"
-	puts " "
 
-	puts "     |     |     "
-	puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}    "	    
-	puts "     |     |     "
-	puts "-----------------"
-	puts "     |     |     "
-	puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}    "
-	puts "     |     |     "
-	puts "-----------------"
-	puts "     |     |     "
-	puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}    "
-	puts "     |     |     "
+def display_board(brd)
+	puts "************************"
+	puts "*        BOARD         *"
+	puts "*        -----         *"
+	puts "*                      *"
+	puts "*       |     |        *"
+	puts "*    #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}     *"	    
+	puts "*       |     |        *"
+	puts "*  -----------------   *"
+	puts "*       |     |        *"
+	puts "*    #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}     *"
+	puts "*       |     |        *"
+	puts "*  -----------------   *"
+	puts "*       |     |        *"
+	puts "*    #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}     *"
+	puts "*       |     |        *"
+	puts "*                      *"
+	puts "************************"
 end
 
 def initilize_board
@@ -28,12 +35,47 @@ end
 
 
 def computer_ki(board)
-	marked_spuare = 0
-	
-	board[]	
-
+	valid_input = false
+	loop do 	
+		computer_input = rand(1..9)
+		if board[computer_input] == " "
+			board[computer_input] = COMPUTER_MARKER
+			valid_input = true
+		end
+		break if valid_input == true
+	end
 end
 
+def player_marks_squer(board, player)
+	
+	valid_player_input = false
+		
+	if player == "1"
+		puts " "
+		puts ">> player 1 please mark a square"
+		puts " "
+	elsif player == "2"
+		puts " "
+		puts ">> player 2 please mark a square"
+		puts " "
+	end
+		loop do 
+			break if valid_player_input == true	
+
+			user_input = gets.chomp.to_i
+			
+			if board[user_input] == " "
+				if player == "1"
+					board[user_input] = PLAYER_1_MARKER
+				elsif player == "2"
+					board[user_input] = PLAYER_2_MARKER
+				end	
+				valid_player_input = true
+			else   
+				puts "square already marked.. please mark a empty square"
+			end
+		end
+end
 
 
 def game_controler_winner(board)
@@ -67,7 +109,7 @@ def game_controler_boardfull(board)
 end
 
 
-def winner_congrat(winner)
+def display_winner_congrat(winner)
 	if winner == "computer"
 		puts " ----------------------"
 		puts "Computer won the game.."
@@ -76,29 +118,77 @@ def winner_congrat(winner)
 
 	elsif winner == "player"
 		puts "---------------------"
-		puts "You are the winner!!!"
+		puts "Player 1 is the winner!!!"
+		puts "---------------------"
+		puts " "
+	elsif winner == "player_2"
+		puts "---------------------"
+		puts "Player 2 is the winner!!!"
 		puts "---------------------"
 		puts " "
 	end
 end
 
 
-def tie_boardfull
+def display_tie_boardfull
 	puts "----------"
 	puts "Its a Tie!"
 	puts "----------"
 	puts " "
 end
 
-def computer_mark
+def display_computer_mark
 	puts " "
 	puts "computer marks a square.."
 	puts " "
-	puts "___________________________________________"
-	puts "___________________________________________"
-	puts " "
 end
 
+def display_round_cut
+	puts "__________________________________________________"
+	puts "__________________________________________________"
+end
+
+def choose_gamemodi
+	puts " "
+	puts "%%%%%%%%%%%%%%%%%%%%%% "
+	puts "GAMEMODI"
+	puts "----------------------"
+	puts "1. Player vs Player"
+	puts " "
+	puts "2. Player vs Computer"
+	puts "----------------------"
+	puts "%%%%%%%%%%%%%%%%%%%%%%"
+	puts " "
+	valid_input = false
+	loop do 
+		user_input = gets.chomp
+		if user_input == "1"
+			puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+			puts "# You choose Gamemodi: Player vs Player #"
+			puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+			valid_input == true
+			return "1"
+		elsif user_input == "2"
+			puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+			puts "# You choose Gamemodi: Player vs Computer #"
+			puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+			valid_input == true
+			return "2"
+		end
+	end	
+end
+
+def display_msg
+	puts promt = <<-MSG
+
+					(1)|(2)|(3)
+					-----------
+					(4)|(5)|(6)
+					-----------
+					(7)|(8)|(9)
+
+  MSG
+end
 # MAIN
 
 
@@ -106,110 +196,108 @@ replay = true
 
 # game loop
 loop do 
-
 	break if replay == false
 
 	board = initilize_board # creats a empty board
 	
-	winner_player = false
-	
-	winner_computer = false
-	
+
+	winner_player_1 = false
+	winner_opponent = false
 	board_full = false
+
+	agianst_player = false
+	agianst_computer = false
 	
 	computer_input = 0
 
-	startpoint = false
+	
+	gamemodi = choose_gamemodi
+	if gamemodi == "1"
+		agianst_player = true
+	else
+		agianst_computer = true
+	end
 
 	# Round loop
 	loop do 
 		
-
+		break if winner_player_1 == true || winner_opponent == true || board_full == true 
+		display_msg
 		display_board(board)
 		
-		break if winner_player == true || winner_computer == true || board_full == true
+		
 
 		
-		# player marks square
-		#------------------------------------------------------------------------
-		puts "please mark square.."
+		# player one marks square
+		#-----------------------------------------------------------------------
 		
-		# valid_player_input loop 
-		valid_player_input = false
-		loop do 
-
-			user_input = gets.chomp.to_i
-			if board.key?(user_input) && board[user_input] == " "
-				board.each do|k,v| 
-					board[user_input] = "X"
-				end	
-				valid_player_input = true
-			else   
-				valid_player_input = false
-				puts "square already marked.. please mark a empty square"
-			end
-
-
-			break if valid_player_input == true	
-		end
+		player_marks_squer(board,"1")
 		
 		#------------------------------------------------------------------------
 
-		
-		if game_controler_winner(board) == "player" # chek if player won the game and if thats the case round break condition set to true
-			winner_player = true
-		end
-		
-
-		board_full = game_controler_boardfull(board) # chek if board allready full
-
-		
-		# computer marks square
+		#check if player already won the game because player 1 is always the beginner. if thats the case opponents turn will skiped.
 		#------------------------------------------------------------------------
-		computer_mark
 		
-		# computer ki starts after startpoint is set
-		if startpoint == true
-		  computer_input = computer_ki(board)
+		if game_controler_winner(board) == "player" 
+			winner_player_1 = true
 		end
-
-		#set start point
-		loop do 
-			break if startpoint == true
-			loop do 
-				break if board[computer_input] == " "
-			  computer_input = rand(1..9)
-			  startpoint = true
-			end
-		end
-
-		board.each{|k,v| board[k] = "O" if (computer_input == k && v == " ")}
-
-
 		
-		if board_full == false # when board already full computer dont try to mark
-			loop do 
-				computer_input = rand(1..9) 
-				break if board[computer_input] == " " 
-			end
-		end
-		 
 		#-------------------------------------------------------------------------
 
+
+		# check if board already full. if thats the case the opponents turn will be skiped
+		#-------------------------------------------------------------------------
 		
-		# check if computer won the game and if thats the case round break conditon is set to true and winner congrats is displayed
-		if game_controler_winner(board) == "computer"
-			winner_congrat("computer")
-			winner_computer = true
-		elsif winner_player == true
-			winner_congrat("player")
+		board_full = game_controler_boardfull(board) # chek if board allready full
+		
+		#-------------------------------------------------------------------------
+
+		display_board(board)
+
+		
+		# opponent marks a square (computer or player 2 depends on what gamemode is selected)
+		#------------------------------------------------------------------------
+		
+		if winner_player_1 == false
+			if agianst_player == false
+				display_computer_mark
+				computer_ki(board)
+			elsif agianst_player == true
+				player_marks_squer(board,"2")
+			end
+		end
+
+		#-------------------------------------------------------------------------
+
+
+		display_round_cut
+
+		
+		# check who won the game and print congrat message + set the breakcondition to true
+		#--------------------------------------------------------------------------
+
+		if game_controler_winner(board) == "computer" && agianst_player == false
+			display_winner_congrat("computer")
+			winner_opponent = true
+		elsif game_controler_winner(board) == "computer" && agianst_player == true
+			display_winner_congrat("player_2")
+			winner_opponent = true
+		elsif game_controler_winner(board) == "player"
+			display_winner_congrat("player")
+			winner_player_1 = true
 		end
 		
-		# Board check if it is full(Tie)
+		#---------------------------------------------------------------------------
+
+		# check if the board is full and if thats the case print tie message and set the round break condition to true
+		#---------------------------------------------------------------------------
+		
 		board_full = game_controler_boardfull(board)
 		if board_full == true
 			tie_boardfull
 		end
+		
+		#---------------------------------------------------------------------------
 
 	end
 
